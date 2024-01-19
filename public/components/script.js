fa2c93b94e7362e75dce515cb585d2e3f2ce7618
ydkulks <ydkulks2@gmail.com>
2024-01-19 20:40:21 +0530

    Added loading-svg custom component

--- public/components/script.js
+++ public/components/script.js
@@ -21,6 +21,21 @@ class Nav extends HTMLElement{
 
 customElements.define('nav-bar', Nav);
 
+// 2
+class Loading extends HTMLElement{
+  constructor(){
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+        <p class="flex justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><circle cx="4" cy="12" r="3" fill="currentColor"><animate id="svgSpinners3DotsBounce0" attributeName="cy" begin="0;svgSpinners3DotsBounce1.end+0.25s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="12" cy="12" r="3" fill="currentColor"><animate attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.1s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="20" cy="12" r="3" fill="currentColor"><animate id="svgSpinners3DotsBounce1" attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.2s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle></svg>
+      </p>
+    `;
+  }
+}
+customElements.define('loading-svg', Loading);
+
 // Counter logic to get current value of the counter
 const currentCount = () => {
   var count = document.getElementById("count").innerText;
